4deec92e5c1859610e06a9798ed5e497322613b7
Attila Kasza <9ttila.kasza@gmail.com>
2020-05-22 08:35:04 +0200

    Updates

---
+++ precache-manifest.7132041c30446de84498209364384fc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29dcb00f74bd1ae2da91fec2cf2b1728",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "dba75fc2a2f2ef75459a",
+    "url": "/react-todo/static/css/main.add1459c.chunk.css"
+  },
+  {
+    "revision": "4ba9dcc36575a81a62e9",
+    "url": "/react-todo/static/js/2.dd851caa.chunk.js"
+  },
+  {
+    "revision": "68ba67984c1bee7feb3697fd5afffb10",
+    "url": "/react-todo/static/js/2.dd851caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba75fc2a2f2ef75459a",
+    "url": "/react-todo/static/js/main.d0a581b6.chunk.js"
+  },
+  {
+    "revision": "68172ead1f0e2993076f",
+    "url": "/react-todo/static/js/runtime-main.a53a9e8b.js"
+  },
+  {
+    "revision": "704f1115e0cfb6ed8f3d9a8d81f08518",
+    "url": "/react-todo/static/media/icon.704f1115.svg"
+  }
+]);
